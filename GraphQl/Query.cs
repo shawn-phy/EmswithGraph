9677207fa9f7ba218ddd63029f42026d25f828c1
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 15:26:15 +0300

    feat: updated query list to return proper services.

---
+++ GraphQl/Query.cs
@@ -0,0 +1,7 @@
+﻿using databaseService;
+
+public class Query
+{
+    public List<Event> GetEvents([Service] IEventService eventService) => eventService.GetEvents();
+    public Event GetEvent(int eventId, [Service] IEventService eventService) => eventService.GetEvent(eventId);
+}
