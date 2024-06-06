130268a7a5eb0357e90274c3f9d919baeaf2df48
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 15:14:43 +0300

    feat: updated attendee model

--- Models/Atendees.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmswithGraph.Models
-{
-    public class Attendees
-    {
-        public int AttendeeId { get; set; }
-        public string Name { get; set; }
-        public int EventId { get; set; }
-    }
-}
---
+++ Models/Event.cs
@@ -0,0 +1,31 @@
+﻿public class Event
+{
+    public int EventId { get; set; }
+    public string? EventName { get; set; }
+    public DateTime EventDate { get; set; }
+    public TimeSpan EventTime { get; set; }
+    public string? Location { get; set; }
+    public string? Description { get; set; }
+    public int OrganizerId { get; set; }
+    public Organizer Organizer { get; set; }
+    public List<Attendee> Attendees { get; set; }
+}
+
+public class Organizer
+{
+    public int OrganizerId { get; set; }
+    public string? OrganizerName { get; set; }
+    public string? ContactEmail { get; set; }
+    public string? ContactPhone { get; set; }
+}
+
+public class Attendee
+{
+    public int AttendeeId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string?   Phone { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    public int EventId { get; set; }
+}
