dad7d3eb9fe62d163868ee3687361d59de2cb303
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 15:36:23 +0300

    feat: added graphql endpoint.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,17 @@
+using databaseService;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container
+builder.Services.AddSingleton<IEventService, EventService>();
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseRouting();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGraphQL();
 
 app.Run();
