6fd3938e7e983dd87a1dd81c197850ff9f4e37e9
shawn-phy <philipoyuga14@gmail.com>
2024-06-06 15:27:24 +0300

    feat: changed mutations to return correct types

---
+++ GraphQl/Mutation.cs
@@ -0,0 +1,17 @@
+﻿public class Mutation
+{
+    public int AddEvent(Event newEvent, [Service] IEventService eventService)
+    {
+        return eventService.AddEvent(newEvent);
+    }
+
+    public int UpdateEvent(Event updatedEvent, [Service] IEventService eventService)
+    {
+        return eventService.UpdateEvent(updatedEvent);
+    }
+
+    public int DeleteEvent(int eventId, [Service] IEventService eventService)
+    {
+        return eventService.DeleteEvent(eventId);
+    }
+}
